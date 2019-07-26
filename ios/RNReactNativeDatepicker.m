2d799fa33783d85b2406c4aa3d7cd05599e1530b
Nguyen Van Nhat <nhat292@gmail.com>
2019-07-27 00:11:14 +0700

    update library

---
+++ ios/RNReactNativeDatepicker.m
@@ -0,0 +1,13 @@
+
+#import "RNReactNativeDatepicker.h"
+
+@implementation RNReactNativeDatepicker
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
